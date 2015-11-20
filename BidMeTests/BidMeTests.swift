8a183c1b00a65b699c62a289b1c94597374fe23f
LipingSun <liping.sun@sjsu.edu>
2015-11-19 16:52:21 -0800

    Initial Commit

---
+++ BidMeTests/BidMeTests.swift
@@ -0,0 +1,36 @@
+//
+//  BidMeTests.swift
+//  BidMeTests
+//
+//  Created by Liping on 11/19/15.
+//  Copyright © 2015 CMPE277. All rights reserved.
+//
+
+import XCTest
+@testable import BidMe
+
+class BidMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
