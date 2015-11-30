a8ac8ef872fc183cdded159ed1f6ca888c55a3b0
LipingSun <liping.sun@sjsu.edu>
2015-11-30 14:38:17 -0800

    Implement login

---
+++ BidMe/Controllers/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  BidMe
+//
+//  Created by Liping on 11/30/15.
+//  Copyright © 2015 CMPE277. All rights reserved.
+//
+
+import UIKit
+import AVOSCloud
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+        AVUser.logInWithUsernameInBackground(userName.text, password: password.text, block: {(user: AVUser?, error: NSError?) in
+            if let user = user {
+                print("login success!")
+            } else {
+                print(error)
+            }
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
