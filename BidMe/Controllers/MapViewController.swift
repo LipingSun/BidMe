875b55110e08496f499cc4d21a210e9533409643
dexterwei <dexterwei@163.com>
2015-12-02 01:38:04 -0800

    add image to annotation view 1/2

--- BidMe/Controllers/MapViewController.swift
+++ BidMe/Controllers/MapViewController.swift
@@ -10,28 +10,30 @@ import UIKit
 import MapKit
 import AVOSCloud
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController,MKMapViewDelegate {
     
     @IBOutlet weak var BidMapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        BidMapView.delegate = self
+        
         let initialLocation = CLLocation(latitude: 37.3883809, longitude:-121.8834904)
         centerMapOnLocation(initialLocation)
         
-        BidMapView.delegate = self
+        
 //        let sample_event = AuctionEvent(title: "Xbox One",
 //            locationName: "Pacific Rim Plaza",
 //            category: "Game",
 //            coordinate: CLLocationCoordinate2D(latitude: 37.386491, longitude: -121.8842214))
         
         let sample_auction = Auction()
-        //sample_auction.status = "something"
+        
         let item = Item()
         item.name = "Xbox"
         item.desc = "270$"
-        //sample_auction.item = item
+        sample_auction.item = item
         sample_auction.location = AVGeoPoint(latitude: 37.386491, longitude: -121.8842214)
         
         let sample_event = AuctionEvent(auction: sample_auction)
@@ -47,6 +49,24 @@ class MapViewController: UIViewController {
         BidMapView.setRegion(coordinateRegion, animated: true)
     }
     
-    
+    func MapView(mapView: MKMapView!,
+        viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+            if (annotation is MKUserLocation) { return nil }
+            
+            let reuseID = "chest"
+            var v = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseID)
+            
+            if v != nil {
+                v?.annotation = annotation
+            } else {
+                v = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            }
+            v!.image = UIImage(named: "dril")
+//            let annotationview = MKAnnotationView(annotation: annotation, reuseIdentifier: "new")
+//            print("Here")
+//            annotationview.image = UIImage(named: "moto")
+//            annotationview.highlighted = true
+//
+            return v
+    }
 }
-
