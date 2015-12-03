cf4815c832d1031232b3f39faa52c6f4f21b0adf
LipingSun <liping.sun@sjsu.edu>
2015-12-03 14:53:48 -0800

    Get auction list synchronously

--- BidMe/Controllers/AuctionListViewController.swift
+++ BidMe/Controllers/AuctionListViewController.swift
@@ -7,16 +7,44 @@
 //
 
 import UIKit
-
-var itemList = ["Printer", "Monitor", "Table", "Chair"]
-var imageList = [UIImage(named: "puppy1"), UIImage(named: "puppy2"), UIImage(named: "puppy3"), UIImage(named: "puppy4")]
+import AVOSCloud
 
 class AuctionListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var itemList = ["Printer", "Monitor", "Table", "Chair"]
+    var imageList = [UIImage(named: "puppy1"), UIImage(named: "puppy2"), UIImage(named: "puppy3"), UIImage(named: "puppy4")]
+    
+    var auctions:[Auction] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setupLeftMenuButton()
+        
+//        let user = AVUser.currentUser() as! User
+//        let img = AVFile(name: "bid.png", data: UIImagePNGRepresentation(UIImage(named: "bid")!))
+//        img.saveInBackgroundWithBlock({(succeeded: Bool, error: NSError?) in
+//            let item = Item(name: "bid", desc: "This is bid", picture: img, owner: user)
+//            let auction = Auction(item: item, startPrice: 30)
+//            auction.saveInBackgroundWithBlock({(succeeded: Bool, error: NSError?) in
+//                if (succeeded) {
+//                    print("good!")
+//                }
+//            })
+//        })
+        
+//        Auction.query().findObjectsInBackgroundWithBlock({(objects: [AnyObject]?, error: NSError?) in
+//            if let auctions = objects as? [Auction] {
+//                for auction in auctions {
+//                    itemList.append(auction.item!.name! as String)
+//                }
+//            }
+//        })
+        let auctionQuery = Auction.query()
+        auctionQuery.includeKey("item.picture")
+        self.auctions = auctionQuery.findObjects() as! [Auction]
+
+//        self.auctions = Auction.query().findObjects() as! [Auction]
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,22 +61,45 @@ class AuctionListViewController: UIViewController, UITableViewDelegate, UITableV
         self.mm_drawerController.toggleDrawerSide(MMDrawerSide.Left, animated: true, completion: nil)
     }
     
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        var cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
+//        
+//        cell.imageView?.image = imageList[indexPath.row]
+//        cell.textLabel?.text = auctions[indexPath.row].item!.name as? String
+//        
+//        return cell
+//    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemList.count
+        return self.auctions.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
         
-        cell.imageView?.image = imageList[indexPath.row]
-        cell.textLabel?.text = itemList[indexPath.row]
+//        Auction.query().findObjectsInBackgroundWithBlock({(objects: [AnyObject]?, error: NSError?) in
+//            if let auctions = objects {
+//                self.auctions = auctions as! [Auction]
+//                
+//                cell.imageView?.image = self.imageList[indexPath.row]
+//                //        cell.textLabel?.text = itemList[indexPath.row]
+//                cell.textLabel?.text = self.auctions[indexPath.row].item?.name as? String
+//            }
+//        })
+        
+        
+        cell.imageView?.image = UIImage(data: self.auctions[indexPath.row].item!.picture!.getData())
+        cell.textLabel?.text = String(self.auctions[indexPath.row].item!.name!)
         
         return cell
+        
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         self.performSegueWithIdentifier("showDetail", sender: self)
     }
+    
+    
 
 }
 
