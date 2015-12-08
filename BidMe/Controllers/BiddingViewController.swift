da75382834f3ef7ab154d3f58343376d4cc01c95
LipingSun <liping.sun@sjsu.edu>
2015-12-07 17:44:26 -0800

    Update auction continuously

--- BidMe/Controllers/BiddingViewController.swift
+++ BidMe/Controllers/BiddingViewController.swift
@@ -7,20 +7,50 @@
 //
 
 import UIKit
+import AVOSCloud
 
 class BiddingViewController: UIViewController {
+
+    var auction: Auction?
+
+    var timer = NSTimer()
+
+    @IBOutlet var itemName: UILabel!
+
+    @IBOutlet var currentPrice: UILabel!
+
+    @IBOutlet var timeLeft: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    func update() {
+
+        if let auction = auction {
+            auction.refresh()
+            auction.item!.fetch()
+            if let auctionItemName = auction.item?.name {
+                itemName.text = "Item: " + String(auctionItemName)
+            }
+            if let auctionCurrentPrice = auction.currentPrice {
+                currentPrice.text = "Current Price: " + String(auctionCurrentPrice)
+            }
+            if let auctionEndTime = auction.endTime {
+                let dateComponentsFormatter = NSDateComponentsFormatter()
+                dateComponentsFormatter.unitsStyle = NSDateComponentsFormatterUnitsStyle.Full
+                timeLeft.text = "Time Left: " + String(dateComponentsFormatter.stringFromTimeInterval(auctionEndTime.timeIntervalSinceNow)!)
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
