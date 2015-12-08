f0dc24dedf5b9da0f787b9a085b13440c79c94ed
LipingSun <liping.sun@sjsu.edu>
2015-12-07 17:10:59 -0800

    set initial current price

--- BidMe/Controllers/AuctionViewController.swift
+++ BidMe/Controllers/AuctionViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVOSCloud
 
 class AuctionViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    
+
     //MARK: properties
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var priceField: UITextField!
@@ -18,86 +18,88 @@ class AuctionViewController: UIViewController, UINavigationControllerDelegate, U
     @IBOutlet weak var startTime: UIDatePicker!
     @IBOutlet weak var endTime: UIDatePicker!
     @IBOutlet weak var desField: UITextView!
-    
-    
-    
+
+
     @IBAction func publishItem(sender: AnyObject) {
-        
-        
+
+
         //create an item
         let item = Item()
-        item.name=nameField.text
-        item.desc=desField.text
-        
+        item.name = nameField.text
+        item.desc = desField.text
+
         let imageSize = imgView.image?.size
         let scale = imageSize!.width / 150
-        
+
         item.picture = AVFile(name: String(item.name!) + ".png", data: UIImagePNGRepresentation(imageWithImage(imgView.image!, scaledToSize: CGSize(width: (imageSize?.width)! / scale, height: (imageSize?.height)! / scale))))
-        
+
         //create an auction object
         let auctionObject = Auction()
         let price = (priceField.text! as NSString).floatValue
-        auctionObject.item=item
-        auctionObject.startPrice=price
-        auctionObject.startTime=startTime.date
-        auctionObject.endTime=endTime.date
-        auctionObject.location=AVGeoPoint(latitude: 37.332224, longitude: -121.869382)
+        auctionObject.item = item
+        auctionObject.startPrice = price
+        auctionObject.currentPrice = price
+        auctionObject.startTime = startTime.date
+        auctionObject.endTime = endTime.date
+        auctionObject.location = AVGeoPoint(latitude: 37.332224, longitude: -121.869382)
 
-        item.picture!.saveInBackgroundWithBlock({(succeeded: Bool, error: NSError?) in
+        item.picture!.saveInBackgroundWithBlock({
+            (succeeded: Bool, error: NSError?) in
             auctionObject.saveInBackground()
             let centerStoryboard: UIStoryboard = UIStoryboard(name: "Center", bundle: nil)
             let centerViewController = centerStoryboard.instantiateViewControllerWithIdentifier("AuctionListViewController")
             self.showViewController(centerViewController, sender: self)
         })
     }
-    
-    func imageWithImage(image:UIImage, scaledToSize newSize:CGSize) -> UIImage{
+
+    func imageWithImage(image: UIImage, scaledToSize newSize: CGSize) -> UIImage {
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
         image.drawInRect(CGRectMake(0, 0, newSize.width, newSize.height))
-        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return newImage
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         //dismiss keyboard when tap outside the specific view
         let dismissKeyboard: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(dismissKeyboard)
-        
-        
+
+
         //add gesture recognizer on image view
-        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("imageTapped:"))
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("imageTapped:"))
         imgView.userInteractionEnabled = true
         imgView.addGestureRecognizer(tapGestureRecognizer)
     }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func imageTapped(img: AnyObject)
-    {
+
+    func imageTapped(img: AnyObject) {
         let image = UIImagePickerController()
         image.delegate = self
-        image.sourceType=UIImagePickerControllerSourceType.PhotoLibrary
-        self.presentViewController(image,animated: true, completion:nil)
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(image, animated: true, completion: nil)
     }
+
     func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
-    
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        let theInfo:NSDictionary=info as NSDictionary
-        let img:UIImage = theInfo.objectForKey(UIImagePickerControllerOriginalImage) as! UIImage
-        imgView.image=img
-        
+
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String:AnyObject]) {
+        let theInfo: NSDictionary = info as NSDictionary
+        let img: UIImage = theInfo.objectForKey(UIImagePickerControllerOriginalImage) as! UIImage
+        imgView.image = img
+
         self.dismissViewControllerAnimated(true, completion: nil)
-        
+
     }
-    
+
 }
 
