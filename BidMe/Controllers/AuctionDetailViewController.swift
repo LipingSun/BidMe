fcb8c48ec30a12ea195b675ed7bf68e105b63413
LipingSun <liping.sun@sjsu.edu>
2015-12-13 00:25:41 -0800

    Implement auction detail scene

--- BidMe/Controllers/AuctionDetailViewController.swift
+++ BidMe/Controllers/AuctionDetailViewController.swift
@@ -9,45 +9,69 @@
 import UIKit
 
 class AuctionDetailViewController: UIViewController {
-    
+
     @IBOutlet weak var ItemDetailLabel: UILabel!
-    
+
     @IBOutlet weak var ItemDetailImage: UIImageView!
-    
+
     @IBOutlet var ItemDetailDesc: UILabel!
+
+
+    // This part is newly added
+
+    @IBOutlet var ItemDetailCurrentPrice: UILabel!
+
+    @IBOutlet var ItemDetailStartPrice: UILabel!
+
+    @IBOutlet var ItemDetailStartTime: UILabel!
+
+    @IBOutlet var ItemDetailEndTime: UILabel!
     
-    @IBOutlet var ItemDetailPrice: UILabel!
-    
-    var passedValue:String?
-    
-    var passedImage:UIImage?
-    
+    @IBOutlet var ItemDetailsOwnerIcon: UIImageView!
+    //--------------------------------------
+
+    //var passedValue:String?
+
+    //var passedImage:UIImage?
+
     var auction: Auction?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeZone = NSTimeZone(name: "PST")
+
         if let auction = auction {
             ItemDetailLabel.text = String(auction.item!.name!)
             ItemDetailImage.image = UIImage(data: (auction.item!.picture!.getData())!)
             ItemDetailDesc.text = "Description: " + String(auction.item!.desc!)
-            ItemDetailPrice.text = "Current Price: " + String(auction.startPrice!)
+            ItemDetailCurrentPrice.text = "Current: $" + String(auction.currentPrice!)
+            ItemDetailStartPrice.text = "Start: $" + String(auction.startPrice!)
+            
+            ItemDetailStartTime.text = "Start Time: " + dateFormatter.stringFromDate(auction.startTime!)
+            ItemDetailEndTime.text = "End Time:   " + dateFormatter.stringFromDate(auction.endTime!)
         }
-
+        
+        ItemDetailsOwnerIcon.layer.cornerRadius = ItemDetailsOwnerIcon.frame.size.width / 2
+        ItemDetailsOwnerIcon.clipsToBounds = true
+        ItemDetailsOwnerIcon.layer.borderWidth = 3.0
+        ItemDetailsOwnerIcon.layer.borderColor = UIColor.whiteColor().CGColor
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func bidButtonTapped(sender: AnyObject) {
         let storyboard = UIStoryboard(name: "Bidding", bundle: nil)
         let biddingViewController = storyboard.instantiateViewControllerWithIdentifier("BiddingViewController") as! BiddingViewController
         biddingViewController.auction = auction
         self.showViewController(biddingViewController, sender: self)
     }
-    
+
     /*
     // MARK: - Navigation
     
@@ -57,5 +81,5 @@ class AuctionDetailViewController: UIViewController {
     // Pass the selected object to the new view controller.
     }
     */
-    
+
 }
